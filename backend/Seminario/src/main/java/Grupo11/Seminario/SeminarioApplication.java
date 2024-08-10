a43b6e367a484ecbe900ebf98f1b126f962eb832
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-08-10 00:11:00 -0300

    chore(backend): Se agrego la carpeta backend del proyecto

---
+++ backend/Seminario/src/main/java/Grupo11/Seminario/SeminarioApplication.java
@@ -0,0 +1,13 @@
+package Grupo11.Seminario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarioApplication.class, args);
+	}
+
+}
