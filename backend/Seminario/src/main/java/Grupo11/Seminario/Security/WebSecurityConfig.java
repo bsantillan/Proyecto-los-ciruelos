63447b3d657c17d3f7de0e7c222de35c931c64df
Santillan Bruno <brunosaantillan@gmail.com>
2024-08-21 11:41:37 -0300

    feat(WebSecurityConfig.java): Se agrego la funcionalidad de dar autenticaciónmediante Tokens de FireBase

---
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Security/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package Grupo11.Seminario.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests -> 
+                authorizeRequests
+                    .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> 
+                oauth2.jwt(Customizer.withDefaults())
+            );
+        return http.build();
+    }
+}
