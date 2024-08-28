00538223e14e0b68b351b0fb2f587c34420c5e93
Santillan Bruno <brunosaantillan@gmail.com>
2024-08-28 15:27:11 -0300

    feat(RegistroService.java): Se agrego la funcion para verificar si existe un usuario con un mail

--- backend/Seminario/src/main/java/Grupo11/Seminario/Service/RegistroService.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Service/RegistroService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import Grupo11.Seminario.Entities.Jugador;
 import Grupo11.Seminario.Entities.Enum.Categoria;
 import Grupo11.Seminario.Repository.IJugadorRepository;
+import Grupo11.Seminario.Repository.IUsuarioRepository;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -14,11 +15,15 @@ public class RegistroService {
 
     @Autowired
     IJugadorRepository i_jugador_repository;
+    @Autowired
+    IUsuarioRepository i_usuario_repository;
 
     // Se guarda el jugador en la BD
     public void guardar_jugador(Jugador jugador){
         i_jugador_repository.save(jugador);
     }
 
-    
+    public Boolean verificar_email(String email){
+        return i_usuario_repository.findByEmail(email);
+    }
 }
