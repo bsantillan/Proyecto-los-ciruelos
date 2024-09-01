e2b1692ee750a3a6f7ffb37762d7826896b131d1
Santillan Bruno <brunosaantillan@gmail.com>
2024-09-01 17:19:56 -0300

    feat(WebSecurityConfig.java): Se agrego el acceso a EndPoints publicos y a EndPoints que requieren autenticacion

--- backend/Seminario/src/main/java/Grupo11/Seminario/Security/WebSecurityConfig.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package Grupo11.Seminario.Security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
     
     @Bean
@@ -16,11 +23,25 @@ public class WebSecurityConfig {
             .authorizeHttpRequests(authorizeRequests -> 
                 authorizeRequests
                 .requestMatchers("/public/**").permitAll() // Permite acceso sin autenticación a "/public/**"
-                .anyRequest().authenticated()
+                .requestMatchers("/privado/**").authenticated()
             )
-            .oauth2ResourceServer(oauth2 -> 
-                oauth2.jwt(Customizer.withDefaults())
-            );
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Habilita CORS
+            .csrf(csrf->csrf.disable()); // Opcional: desactiva CSRF si no es necesario
+            http.oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
