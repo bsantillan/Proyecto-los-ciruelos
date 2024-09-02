0cfd4c14b7c0327ae8be74394cee229d11f28e3f
Santillan Bruno <brunosaantillan@gmail.com>
2024-09-02 19:08:55 -0300

    feat(RegistroPrivadoController.java): Se agrego el controlador para registrar a un empleado

---
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Controller/RegistroPrivadoController.java
@@ -0,0 +1,14 @@
+package Grupo11.Seminario.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/private/registro")
+public class RegistroPrivadoController {
+    
+    
+}
