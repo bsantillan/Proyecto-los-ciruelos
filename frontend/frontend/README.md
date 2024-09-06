e047f2ecae953c1a19dbf1c3be494ae6ad35e513
Katerina Mariescurrena <katyfacultad@gmail.com>
2024-09-06 20:45:13 -0300

    doc(Readme): Agregue informacion

--- frontend/frontend/README.md
+++ frontend/frontend/README.md
@@ -1,6 +1,27 @@
+
+# Frontend Los ciruelos
+
+
+
+## Instalacion
+### Boostrap
 npm install bootstrap jquery @popperjs/core
 
+npm install bootstrap
+
+### Angular
+
 npm install -g @angular/cli
+
+ng add @angular/material --skip-confirmation
+
+### Firebase
+
+ng add @angular/fire
+
+npm install -g firebase-tools
+
+## Adicionales
 
 npm install
 
@@ -12,7 +33,31 @@ ng new frontend --standalone=false
 Crear enviroments:
 ng generate environments
 
-Prueba de Mercado Pago:
---cuentas prueba:
-    Comprador: usser:TESTUSER425217600 pass:Rb0gJBVdmK
-    Vendedor: usser:TESTUSER1751008672 pass:ybeYrmZjxX
+### Prueba de Mercado Pago:
+
+Cuentas prueba:
+   
+    -Comprador: usser:TESTUSER425217600 pass:Rb0gJBVdmK
+   
+    -Vendedor: usser:TESTUSER1751008672 pass:ybeYrmZjxX
+
+## Colores del Proyecto
+
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Text Color | #041A0F |
+| Background Color |  #E0F3EA |
+| 2 Background Color |  #EBFFF5 |
+| Button Color |  #2BDA79 |
+| 2 Button Color |  #8CE3B9 |
+| Accent Color |  #E45355 |
+
+
+## Formato de escritura
+
+- snake_case: palabras separadas por barra baja en vez de espacios y 
+- con la primera letra de cada palabra en minúscula. 
+
+Por ejemplo: mi_blog_de_desarrollo.
+
+Este tipo de convención seran utilizados para nombres de variables y funciones.
--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgOptimizedImage } from '@angular/common';
+
+// Importaciones de Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
-import { NgOptimizedImage } from '@angular/common';
 
+
+// Components
 import { AppComponent } from './app.component';
 import { MercadopagoComponent } from './components/mercadopago/mercadopago.component';
 import { CambiarContraseniaComponent } from './components/login/Cambiar-Contrasenia/cambiar-contrasenia.component';
@@ -19,6 +23,11 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ButtonProviders } from './components/login/Cambiar-Contrasenia/button-providers/button-providers.component';
 import { HomeComponent } from './components/home/home.component';
+
+// Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 @NgModule({
@@ -45,7 +54,11 @@ import { HomeComponent } from './components/home/home.component';
     MatSelectModule,
     NgOptimizedImage
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"proyecto-los-ciruelos","appId":"1:458631280275:web:077d19f3d31ac919ca3f66","storageBucket":"proyecto-los-ciruelos.appspot.com","apiKey":"AIzaSyADexIDOi159hPk8yHrKvBrh8n8OeY5Cpo","authDomain":"proyecto-los-ciruelos.firebaseapp.com","messagingSenderId":"458631280275","measurementId":"G-K0V8KZ571Q"})),
+    provideAuth(() => getAuth()),
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
