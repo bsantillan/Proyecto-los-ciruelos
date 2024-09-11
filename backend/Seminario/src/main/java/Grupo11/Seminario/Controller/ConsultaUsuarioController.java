e70f61d8914f9652ecca950b092cbf175e60e61e
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-11 17:12:48 -0300

    feat(ConsultarUsuarioService.java): Se crea el controller del consultar usuarios

---
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Controller/ConsultaUsuarioController.java
@@ -0,0 +1,29 @@
+package Grupo11.Seminario.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import Grupo11.Seminario.Entities.Usuario;
+import Grupo11.Seminario.Service.ConsultaUsuarioService;
+
+@RestController
+@RequestMapping(path = "/private/consultar")
+public class ConsultaUsuarioController {
+
+    @Autowired
+    private ConsultaUsuarioService consultaUsuarioService;
+
+    // Endpoint para buscar usuarios
+    @GetMapping("/usuarios/buscar")
+    public List<Usuario> buscar_usuarios(
+            @RequestParam Optional<String> nombre,
+            @RequestParam Optional<String> apellido,
+            @RequestParam Optional<String> email) {
+        // Invocamos el servicio para buscar usuarios por los filtros
+        return consultaUsuarioService.buscar_usuarios(nombre, apellido, email);
+    }
+}
