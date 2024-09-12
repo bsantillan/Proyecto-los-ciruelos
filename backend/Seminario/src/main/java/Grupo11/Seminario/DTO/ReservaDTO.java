8f418592f05f69a0f5ca26e56f959b12dcf32e24
Santillan Bruno <brunosaantillan@gmail.com>
2024-09-11 21:34:19 -0300

    feat(ReservaDTO.java): Se agrego un atributo

--- backend/Seminario/src/main/java/Grupo11/Seminario/DTO/ReservaDTO.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/DTO/ReservaDTO.java
@@ -14,4 +14,19 @@ public class ReservaDTO {
     private LocalTime horario_inicio;
     private LocalTime horario_fin;
     private Integer numero_cancha;
+    private Integer id_empleado;
+
+    public ReservaDTO
+    (Float precio, 
+    Integer numero_cancha, Integer cantidad_pelotas, Integer cantidad_paletas, Integer id_empleado, 
+    LocalDate fecha, LocalTime horario_inicio, LocalTime horario_fin){
+        this.precio=precio;
+        this.numero_cancha=numero_cancha;
+        this.cantidad_pelotas=cantidad_pelotas;
+        this.cantidad_paletas=cantidad_paletas;
+        this.fecha=fecha;
+        this.horario_inicio=horario_inicio;
+        this.horario_fin=horario_fin;
+        this.id_empleado=id_empleado;
+    }
 }
