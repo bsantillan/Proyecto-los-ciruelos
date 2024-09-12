5412c0fd17352571cf1132dbfb66523051ddfb91
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-12 19:37:40 -0300

    feat(ConsultarUsuarioController.java): Modificacion del codigo para que maneje el UsuarioDTO

--- backend/Seminario/src/main/java/Grupo11/Seminario/Controller/ConsultaUsuarioController.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Controller/ConsultaUsuarioController.java
@@ -2,12 +2,15 @@ package Grupo11.Seminario.Controller;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import Grupo11.Seminario.Entities.Usuario;
+
+import Grupo11.Seminario.DTO.UsuarioDTO;
 import Grupo11.Seminario.Service.ConsultaUsuarioService;
 
 @RestController
@@ -17,13 +20,14 @@ public class ConsultaUsuarioController {
     @Autowired
     private ConsultaUsuarioService consultaUsuarioService;
 
-    // Endpoint para buscar usuarios
+    // Endpoint para buscar usuarios y devolver DTOs
     @GetMapping("/usuarios/buscar")
-    public List<Usuario> buscar_usuarios(
-            @RequestParam Optional<String> nombre,
-            @RequestParam Optional<String> apellido,
-            @RequestParam Optional<String> email) {
-        // Invocamos el servicio para buscar usuarios por los filtros
-        return consultaUsuarioService.buscar_usuarios(nombre, apellido, email);
+    public ResponseEntity<List<UsuarioDTO>> buscarUsuarios(
+        @RequestParam Optional<String> nombre,
+        @RequestParam Optional<String> apellido,
+        @RequestParam Optional<String> email
+    ) {
+        List<UsuarioDTO> usuarios = consultaUsuarioService.buscar_usuarios(nombre, apellido, email);
+        return ResponseEntity.ok(usuarios);
     }
 }
