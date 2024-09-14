0c4133b5731c8364cbef4f58d06729baec2fa3aa
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-14 16:41:54 -0300

    feat(JugadorDTO.java): Se agrego un metodo para convertir un Jugador a un JugadorDTO

--- backend/Seminario/src/main/java/Grupo11/Seminario/DTO/JugadorDTO.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/DTO/JugadorDTO.java
@@ -2,6 +2,7 @@ package Grupo11.Seminario.DTO;
 
 import java.util.List;
 
+import Grupo11.Seminario.Entities.Jugador;
 import Grupo11.Seminario.Entities.Telefono;
 import lombok.Data;
 
@@ -11,16 +12,33 @@ public class JugadorDTO {
     private String nombre;
     private String apellido;
     private String categoria;
+    private Boolean socio;
+    private Boolean profesor;
     private List<Telefono> telefonos;
 
+    // Constructor con argumentos
     public JugadorDTO(
-    String email, String nombre, String apellido, String categoria, List<Telefono> telefonos) 
+    String email, String nombre, String apellido, String categoria, Boolean socio, Boolean profesor, List<Telefono> telefonos) 
     {
         this.email = email;
         this.nombre = nombre;
         this.apellido = apellido;
         this.categoria = categoria;
+        this.socio = socio;
+        this.profesor = profesor;
         this.telefonos = telefonos;
     }
     
+    // Método estático para convertir de Jugador (entidad) a JugadorDTO
+    public static JugadorDTO fromEntity(Jugador jugador) {
+        return new JugadorDTO(
+            jugador.getEmail(),
+            jugador.getNombre(),
+            jugador.getApellido(),
+            jugador.getCategoria().toString(), // Convertimos el enum a String
+            jugador.getSocio(),
+            jugador.getProfesor(),
+            jugador.getTelefonos()
+        );
+    }
 }
