0af675b09e13b29cd0e445df5528770ef91721e2
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-17 15:26:47 -0300

    feat(JugadorController.java): Se agrego la validacion del duenio, si es duenio se le permite asignar el rol y en caso contrario no

--- backend/Seminario/src/main/java/Grupo11/Seminario/Controller/JugadorController.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Controller/JugadorController.java
@@ -2,6 +2,8 @@ package Grupo11.Seminario.Controller;
 
 import Grupo11.Seminario.Entities.Jugador;
 import Grupo11.Seminario.Service.JugadorService;
+import Grupo11.Seminario.Service.RegistroService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +20,23 @@ public class JugadorController {
 
     @Autowired
     private JugadorService jugadorService;
+    @Autowired
+    private RegistroService registroService;
 
     // Asigna el rol de profesor a un jugador
-    @PutMapping("/asignar_profesor/{id}")
-    public ResponseEntity<String> asignarRolProfesor(@PathVariable Integer id) {
-        boolean exito = jugadorService.asignar_rol_profesor(id);
-        if (exito) {
-            return ResponseEntity.ok("Rol de profesor asignado exitosamente al jugador.");
-        } else {
-            return ResponseEntity.badRequest().body("Error al asignar el rol de profesor.");
+    @PutMapping("/asignar_profesor/{jugador_id}/{duenio_id}")
+    public ResponseEntity<String> asignarRolProfesor(@PathVariable Integer jugador_id, @PathVariable Integer duenio_id) {
+        Boolean exito = jugadorService.asignar_rol_profesor(jugador_id);
+        Boolean vduenio = registroService.verificar_duenio(duenio_id);
+        if (vduenio) {
+            
+            if (exito) {
+                return ResponseEntity.ok("Rol de profesor asignado exitosamente al jugador.");
+            } else {
+                return ResponseEntity.badRequest().body("Error al asignar el rol de profesor.");
+            }
         }
+        return ResponseEntity.badRequest().body("Error al verificar que se sea un dueño");
     }
 }
 
