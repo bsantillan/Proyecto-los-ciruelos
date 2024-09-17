ed53d2598c4616a619d44e894ef05e2bed1a9f26
Santillan Bruno <brunosaantillan@gmail.com>
2024-09-17 17:26:44 -0300

    feat(PagoMercadoPagoDTO.java): Se agrego la clase DTO para manejar los datos de Mercado Pago

---
+++ backend/Seminario/src/main/java/Grupo11/Seminario/DTO/PagoMercadoPagoDTO.java
@@ -0,0 +1,41 @@
+package Grupo11.Seminario.DTO;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import Grupo11.Seminario.Entities.Enum.EstadoPago;
+import lombok.Data;
+
+@Data
+public class PagoMercadoPagoDTO {
+    private String email_pagador;
+    private String nombre_pagador;
+    private String apellido_pagador;
+    private String tipo_identificacion;
+    private String numero_identificacion;
+    private String motivo;
+    private String metodo_pago;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private EstadoPago estado;
+
+    public PagoMercadoPagoDTO(
+        String email_pagador, String nombre_pagador, String apellido_pagador, 
+        String tipo_identificacion, String numero_identificacion, 
+        String motivo, String metodo_pago,
+        LocalDate fecha, LocalTime hora, EstadoPago estado) 
+        {
+        this.email_pagador = email_pagador;
+        this.nombre_pagador = nombre_pagador;
+        this.apellido_pagador = apellido_pagador;
+        this.tipo_identificacion = tipo_identificacion;
+        this.numero_identificacion = numero_identificacion;
+        this.motivo = motivo;
+        this.metodo_pago = metodo_pago;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado=estado;
+    }
+
+    
+}
