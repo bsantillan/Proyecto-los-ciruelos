0ceeb26c81aa6ead1bf3e1e4622845b176db8631
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-24 17:29:36 -0300

    feat(ReservaService.java): Se agregaron funciones

--- backend/Seminario/src/main/java/Grupo11/Seminario/Service/ReservaService.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Service/ReservaService.java
@@ -1,10 +1,18 @@
 package Grupo11.Seminario.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import Grupo11.Seminario.DTO.PagoMercadoPagoDTO;
 import Grupo11.Seminario.Entities.Cancha;
+import Grupo11.Seminario.Entities.ConfiguracionGeneral;
+import Grupo11.Seminario.Entities.Empleado;
+import Grupo11.Seminario.Entities.Jugador;
 import Grupo11.Seminario.Entities.Reserva;
 import Grupo11.Seminario.Repository.ICanchaRepository;
 import Grupo11.Seminario.Repository.IReservaRepository;
@@ -17,6 +25,14 @@ public class ReservaService {
     ICanchaRepository i_cancha_repository;
     @Autowired 
     IReservaRepository i_reserva_repository;
+    @Autowired
+    JugadorService jugador_service;
+    @Autowired
+    EmpleadoService empleado_service;
+    @Autowired
+    PagoService pago_service;
+    @Autowired
+    ConfiguracionGeneralService configuracion_general_service;
 
     public void guardar_reserva(Reserva reserva){
         i_reserva_repository.save(reserva);
@@ -26,10 +42,64 @@ public class ReservaService {
         return i_cancha_repository.findByNumero(numero_cancha).get();
     }
 
+    // Se busca si existe el empleado
+    public Boolean existe_empleado(Integer id_empleado){
+        return empleado_service.existe_empleado(id_empleado);
+    }
+
+    // Se busca si existe el jugador
+    public Boolean existe_jugador(Integer id_jugador){
+        return jugador_service.existe_jugador(id_jugador);
+    }
+
+    //Se busca al jugador por su id
+    public Jugador buscar_jugador(Integer id_jugador){
+        return jugador_service.buscar_jugador(id_jugador);
+    }
+
+    //Se busca al empleado por su id
+    public Empleado buscar_empleado(Integer id_empleado){
+        return empleado_service.buscar_empleado(id_empleado);
+    }
+
     public Boolean verificar_numero_cancha(Integer numero_cancha){
         if (i_cancha_repository.findByNumero(numero_cancha).isEmpty()){
             return false;
         }
         return true;
+    }
+
+    public Float calcular_precio_reserva(Boolean socio, Integer cant_pelotas, Integer cant_paletas, Integer cant_medias_horas){
+        ConfiguracionGeneral configuracion_general = configuracion_general_service.get_configuracion_general();
+        if (socio) {
+            return (configuracion_general.getMonto_reserva() + 
+            configuracion_general.getMonto_paletas() * cant_paletas + 
+            configuracion_general.getMonto_pelotas() * cant_pelotas +
+            configuracion_general.getMonto_x_media_hora() * cant_medias_horas) * descuento(socio);
+        }
+        return configuracion_general.getMonto_reserva() + 
+        configuracion_general.getMonto_paletas() * cant_paletas + 
+        configuracion_general.getMonto_pelotas() * cant_pelotas +
+        configuracion_general.getMonto_x_media_hora() * cant_medias_horas;
+    }
+
+    public Float descuento(Boolean socio){
+        ConfiguracionGeneral configuracion_general = configuracion_general_service.get_configuracion_general();
+        if (socio) {
+            return configuracion_general.getDescuento_socio();
+        }
+        return 0f;
+    }
+
+    public Boolean validar_pago(ResponseEntity<String> response) throws JsonMappingException, JsonProcessingException{
+        return pago_service.validar_pago(response);
+    }
+
+    public PagoMercadoPagoDTO pagar(ResponseEntity<String> response) throws JsonMappingException, JsonProcessingException{
+        return pago_service.pagar(response);
+    }
+
+    public ResponseEntity<String> buscar_pago(Long id_mp) throws JsonMappingException, JsonProcessingException{
+        return pago_service.buscar_pago(id_mp);
     }
 }
