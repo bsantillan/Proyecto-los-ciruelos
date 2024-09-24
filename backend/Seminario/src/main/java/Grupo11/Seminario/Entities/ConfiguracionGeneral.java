238666940b1ecceb16bbb5e8d16a52aca49e4c3b
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-24 17:22:39 -0300

    feat(ConfiguracionGeneral.java): Se actualizo los atributos de la entitie

--- backend/Seminario/src/main/java/Grupo11/Seminario/Entities/ConfiguracionGeneral.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Entities/ConfiguracionGeneral.java
@@ -39,14 +39,20 @@ public class ConfiguracionGeneral {
     @Column(nullable = false, name = "monto_pelotas")
     private Float monto_pelotas;
 
+    @Column(nullable = false, name = "duracion_minima_turno")
+    private Float duracion_minima_turno;
+
     @Column(nullable = false, name = "duracion_maxima_turno")
-    private Integer duracion_maxima_turno;
+    private Float duracion_maxima_turno;
 
     @Column(nullable = false, name = "horario_inicio_pico")
     private LocalTime horario_inicio_pico;
 
     @Column(nullable = false, name = "horario_fin_pico")
     private LocalTime horario_fin_pico;
+
+    @Column(nullable = false, name = "monto_x_media_hora")
+    private Float monto_x_media_hora;
 
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(nullable = false, name = "configuracion_general_id")
