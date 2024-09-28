b495a6a3b624dc8d0f7b6576636e58dee793bdfc
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-09-28 18:40:35 -0300

    feat(ReservaDTO.java): Se agrego el atributo senia

--- backend/Seminario/src/main/java/Grupo11/Seminario/DTO/ReservaDTO.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/DTO/ReservaDTO.java
@@ -14,17 +14,19 @@ public class ReservaDTO {
     private LocalTime horario_fin;
     private Integer numero_cancha;
     private Integer id_empleado;
+    private Boolean senia;
     private long id_mp;
 
     public ReservaDTO
     ( Integer numero_cancha, Integer cantidad_pelotas, Integer cantidad_paletas, Integer id_empleado, 
-    LocalDate fecha, LocalTime horario_inicio, LocalTime horario_fin){
+    LocalDate fecha, LocalTime horario_inicio, LocalTime horario_fin, Boolean senia){
         this.numero_cancha=numero_cancha;
         this.cantidad_pelotas=cantidad_pelotas;
         this.cantidad_paletas=cantidad_paletas;
         this.fecha=fecha;
         this.horario_inicio=horario_inicio;
         this.horario_fin=horario_fin;
+        this.senia=senia;
         this.id_empleado=id_empleado;
     }
 }
