cccda1d7213b944fcccbd94f6d7cc39abf2e089e
Santillan Bruno <brunosaantillan@gmail.com>
2024-09-30 21:50:19 -0300

    feat(AsociacionController.java): Se agrego la verificacion de la existencia del jugador

--- backend/Seminario/src/main/java/Grupo11/Seminario/Controller/AsociacionController.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Controller/AsociacionController.java
@@ -44,51 +44,54 @@ public class AsociacionController {
             // Se verifica que sea un dueño
             if (asociacion_service.verificar_duenio(id_duenio)) {
 
-                // Se verifica que el jugador no este asociado
-                if (asociacion_service.verificar_asociacion(asociacionDTO.getId_jugador())) {
-                    ResponseEntity<String> response = pago_service.buscar_pago(asociacionDTO.getId_pago());
+                if (asociacion_service.existe_jugador(asociacionDTO.getId_jugador())) {
+                    // Se verifica que el jugador no este asociado
+                    if (asociacion_service.verificar_asociacion(asociacionDTO.getId_jugador())) {
+                        ResponseEntity<String> response = pago_service.buscar_pago(asociacionDTO.getId_pago());
 
-                    // Se valida que este realizado el pago
-                    if (pago_service.validar_pago(response)) {
-                        Jugador jugador = asociacion_service.buscar_jugador(asociacionDTO.getId_jugador());
-                        jugador.setSocio(true);
+                        // Se valida que este realizado el pago
+                        if (pago_service.validar_pago(response)) {
+                            Jugador jugador = asociacion_service.buscar_jugador(asociacionDTO.getId_jugador());
+                            jugador.setSocio(true);
 
-                        Empleado duenio = asociacion_service.buscar_empleado(id_duenio);
-                        ConfiguracionGeneral configuracion_general = configuracion_general_service.get_configuracion_general();
-                        PagoMercadoPagoDTO pago_mpDTO = pago_service.pagar(response);
+                            Empleado duenio = asociacion_service.buscar_empleado(id_duenio);
+                            ConfiguracionGeneral configuracion_general = configuracion_general_service.get_configuracion_general();
+                            PagoMercadoPagoDTO pago_mpDTO = pago_service.pagar(response);
     
-                        Cuenta cuenta = new Cuenta();
-                        cuenta.setEmail(pago_mpDTO.getEmail_pagador());
-                        cuenta.setApellido(pago_mpDTO.getApellido_pagador());
-                        cuenta.setNombre(pago_mpDTO.getNombre_pagador());
-                        cuenta.setTipo_identificacion(pago_mpDTO.getTipo_identificacion());
-                        cuenta.setNumero_identificacion(pago_mpDTO.getNumero_identificacion());
+                            Cuenta cuenta = new Cuenta();
+                            cuenta.setEmail(pago_mpDTO.getEmail_pagador());
+                            cuenta.setApellido(pago_mpDTO.getApellido_pagador());
+                            cuenta.setNombre(pago_mpDTO.getNombre_pagador());
+                            cuenta.setTipo_identificacion(pago_mpDTO.getTipo_identificacion());
+                            cuenta.setNumero_identificacion(pago_mpDTO.getNumero_identificacion());
     
-                        Pago pago = new Pago();
-                        pago.setFecha(pago_mpDTO.getFecha());
-                        pago.setHora(pago_mpDTO.getHora());
-                        pago.setMetodo(pago_mpDTO.getMetodo_pago());
-                        pago.setMotivo(pago_mpDTO.getMotivo());
-                        pago.setEstado(pago_mpDTO.getEstado());
-                        pago.setCuenta(cuenta);
-                        pago.setMonto(configuracion_general.getMonto_asociacion());
-                        pago.setDescuento(0f);
+                            Pago pago = new Pago();
+                            pago.setFecha(pago_mpDTO.getFecha());
+                            pago.setHora(pago_mpDTO.getHora());
+                            pago.setMetodo(pago_mpDTO.getMetodo_pago());
+                            pago.setMotivo(pago_mpDTO.getMotivo());
+                            pago.setEstado(pago_mpDTO.getEstado());
+                            pago.setCuenta(cuenta);
+                            pago.setMonto(configuracion_general.getMonto_asociacion());
+                            pago.setDescuento(0f);
     
-                        Asociacion asociacion = new Asociacion();
-                        asociacion.setFecha(LocalDate.now());
-                        asociacion.setHora(LocalTime.now());
-                        asociacion.setPrecio(configuracion_general.getMonto_asociacion());
+                            Asociacion asociacion = new Asociacion();
+                            asociacion.setFecha(LocalDate.now());
+                            asociacion.setHora(LocalTime.now());
+                            asociacion.setPrecio(configuracion_general.getMonto_asociacion());
     
-                        asociacion.setJugador(jugador);
-                        asociacion.setDuenio(duenio);
-                        asociacion.setPago(pago);
+                            asociacion.setJugador(jugador);
+                            asociacion.setDuenio(duenio);
+                            asociacion.setPago(pago);
     
-                        asociacion_service.guardar_asociacion(asociacion);
-                        return ResponseEntity.ok().body("Asociacion Exitosa");
+                            asociacion_service.guardar_asociacion(asociacion);
+                            return ResponseEntity.ok().body("Asociacion Exitosa");
+                        }   
+                        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro el pago");
                     }
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro el pago");
+                    return ResponseEntity.badRequest().body("El jugador ya es un socio");
                 }
-                return ResponseEntity.badRequest().body("El jugador ya es un socio");
+                return ResponseEntity.badRequest().body("No existe el jugador");
             } 
             return ResponseEntity.badRequest().body("No tiene permisos para esta accion");  
         }
