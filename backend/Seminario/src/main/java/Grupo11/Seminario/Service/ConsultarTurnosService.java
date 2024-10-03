0163153b301a35544253f4b98d97d09e21b248f6
Santillan Bruno <brunosaantillan@gmail.com>
2024-10-02 21:40:52 -0300

    feat(ConsultarTurnosService.java): Se agrego la logica para devolver los turnos ocupados que no cumplen el horario minimo de reserva

--- backend/Seminario/src/main/java/Grupo11/Seminario/Service/ConsultarTurnosService.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Service/ConsultarTurnosService.java
@@ -1,7 +1,9 @@
 package Grupo11.Seminario.Service;
 
 import java.util.List;
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import Grupo11.Seminario.DTO.TurnoDTO;
 import Grupo11.Seminario.Entities.Cancha;
 import Grupo11.Seminario.Entities.ConfiguracionGeneral;
+import Grupo11.Seminario.Entities.DiaApertura;
 import Grupo11.Seminario.Entities.Turno;
 import Grupo11.Seminario.Repository.ICanchaRepository;
 import Grupo11.Seminario.Repository.IDiaAperturaRepository;
@@ -42,6 +45,25 @@ public class ConsultarTurnosService {
             // Revisar los espacios entre turnos ocupados
             for (int i = 0; i < turnos_por_cancha.size(); i++) {
                 Turno turno_actual = turnos_por_cancha.get(i);
+
+                // Dia de la semana del turno
+                String dia_de_semana = this.dia_espaniol(turno_actual.getFecha());
+
+                DiaApertura dia_apertura = i_dia_apertura_repository.findByDia(dia_de_semana);
+
+                // Difencia en minutos entre el turno y el horario de apertura
+                long minutos_entre_apertura = Duration.between(dia_apertura.getHorario_inicio(), turno_actual.getHorarioInicio()).toMinutes();
+
+                // Ver si hay un espacio estre el turno y el horario de apertura
+                if ( minutos_entre_apertura>0 & minutos_entre_apertura<configuracion_general.getDuracion_minima_turno()) {
+                    turnosDTO.add(new TurnoDTO(
+                        cancha.getId(),
+                        turno_actual.getFecha(),
+                        dia_apertura.getHorario_inicio(),
+                        turno_actual.getHorarioInicio()
+                    ));
+                }
+
                 turnosDTO.add(new TurnoDTO(
                 cancha.getId(),
                 turno_actual.getFecha(),
@@ -67,8 +89,45 @@ public class ConsultarTurnosService {
                         ));
                     }
                 }
+
+                // Difencia en minutos entre el turno y el horario de cierre
+                long minutos_entre_cierre = Duration.between(turno_actual.getHorario_fin(), dia_apertura.getHorario_fin()).toMinutes();
+                
+                // Ver si hay un espacio estre el turno y el horario de cierre
+                if (minutos_entre_cierre>0 & minutos_entre_cierre<configuracion_general.getDuracion_minima_turno()) {
+                    turnosDTO.add(new TurnoDTO(
+                        cancha.getId(),
+                        turno_actual.getFecha(),
+                        turno_actual.getHorario_fin(),
+                        dia_apertura.getHorario_fin()
+                    ));
+                }
             }
         }
         return turnosDTO;
+    }
+
+    public String dia_espaniol(LocalDate fecha){
+        // Dia de la semana del turno
+        DayOfWeek numero_dia_semana = fecha.getDayOfWeek();
+        String dia_de_semana="";
+        switch (numero_dia_semana) {
+            case MONDAY:
+                return dia_de_semana = "Lunes";
+            case TUESDAY:
+                return dia_de_semana = "Martes";
+            case WEDNESDAY:
+                return dia_de_semana = "Miércoles";
+            case THURSDAY:
+                return dia_de_semana = "Jueves";
+            case FRIDAY:
+                return dia_de_semana = "Viernes";
+            case SATURDAY:
+                return dia_de_semana = "Sábado";
+            case SUNDAY:
+                return dia_de_semana = "Domingo";
+            default:
+                return dia_de_semana;
+        }
     }
 }
