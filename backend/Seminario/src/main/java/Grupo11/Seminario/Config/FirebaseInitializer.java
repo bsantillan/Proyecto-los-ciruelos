44d4ca625dcfbe106f3a261169fe581967071786
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-11-03 19:32:22 -0300

    feat: Se agrego todo lo necesario para dockerizar la aplicacion utilizando la autenticacion de firebase

--- backend/Seminario/src/main/java/Grupo11/Seminario/Config/FirebaseInitializer.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/Config/FirebaseInitializer.java
@@ -3,11 +3,11 @@ package Grupo11.Seminario.Config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
-
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Service
 public class FirebaseInitializer {
@@ -15,20 +15,23 @@ public class FirebaseInitializer {
     @PostConstruct
     public void initialize() {
         try {
-            ClassPathResource serviceAccount = new ClassPathResource("serviceAccountKey.json");
+            // Leer la ruta del archivo JSON desde la variable de entorno
+            String firebaseCredentialsPath = System.getenv("FIREBASE_CREDENTIALS_PATH");
+            InputStream serviceAccount = new FileInputStream(firebaseCredentialsPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-            FirebaseApp.initializeApp(options);
-            System.out.println("Firebase inicializado correctamente.");
+            // Inicializa FirebaseApp si no ha sido inicializado
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase inicializado correctamente.");
+            } else {
+                System.out.println("Firebase ya estaba inicializado.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
 }
-
-
-
--- backend/Seminario/src/main/java/Grupo11/Seminario/SeminarioApplication.java
+++ backend/Seminario/src/main/java/Grupo11/Seminario/SeminarioApplication.java
@@ -3,10 +3,14 @@ package Grupo11.Seminario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SeminarioApplication {
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(SeminarioApplication.class, args);
 	}
 
